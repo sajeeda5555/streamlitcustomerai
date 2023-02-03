7b41815dce9739d4fcf18f406da0ba8e8e3e7c81
sajeeda5555 <124336917+sajeeda5555@users.noreply.github.com>
2023-02-03 11:32:01 +0530

    Add files via upload

---
+++ Config.py
@@ -0,0 +1,17 @@
+connection_parameters = {
+
+"account": "anblickspartner.east-us-2.azure",
+
+"user": "IPSERVICE",
+
+"password": "Ipservice@2022",
+
+"role": "ACCOUNTADMIN",
+
+"warehouse": "DEV_WH",
+
+"database": "CUSTOMERAI",
+
+"schema": "MAIN"
+
+}
